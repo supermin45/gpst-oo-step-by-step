1880d828edc9facf2c5a222cb540cf04df5a142e
supermin45 <1299657352@qq.com>
2017-08-16 22:51:30 +0800

    'practice-1-Person'

--- main/practice-1/student.js
+++ main/practice-1/student.js
@@ -1,3 +1,12 @@
 const Person = require('./person');
 
-module.exports = class Student {}
+module.exports = class Student extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    introduce() {
+        return super.introduce() + ' I am a Student. I am at Class ' + this.klass + '.';
+    }
+}
--- main/practice-1/worker.js
+++ main/practice-1/worker.js
@@ -1,3 +1,11 @@
 const Person = require('./person');
 
-module.exports = class Worker {}
+module.exports = class Worker extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+    }
+
+    introduce() {
+        return super.introduce() + ' I am a Worker. I have a job.';
+    }
+}
