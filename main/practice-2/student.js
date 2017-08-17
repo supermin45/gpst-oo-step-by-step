ba4bf8e16e052a7047b5c88893279914795e5914
supermin45 <1299657352@qq.com>
2017-08-17 16:50:28 +0800

    finish practice-2

--- main/practice-2/student.js
+++ main/practice-2/student.js
@@ -1,3 +1,12 @@
 const Person = require('./person');
 
-module.exports = class Student {}
+module.exports = class Student extends Person {
+    constructor(name, age, clazz) {
+        super(name, age);
+        this.clazz = clazz;
+    }
+
+    introduce() {
+        return (this.clazz.leader !== this.id) ? super.introduce() + ' I am a Student. I am at Class ' + this.clazz.number + '.' : super.introduce() + ' I am a Student.' + ' I am Leader of Class ' + this.clazz.number + '.';
+    }
+}
--- main/practice-2/teacher.js
+++ main/practice-2/teacher.js
@@ -1,3 +1,30 @@
 const Person = require('./person');
 
-module.exports = class Teacher {}
+module.exports = class Teacher extends Person {
+    constructor(name, age, clazzes) {
+        super(name, age);
+        this.clazzes = clazzes;
+    }
+
+    introduce() {
+        return super.introduce() + this.getTeacherClass();
+    }
+
+    getTeacherClass() {
+        let result = ' I am a Teacher. ';
+        if (this.clazzes.length === 0) {
+            result += 'I teach No Class.';
+        } else {
+            result += 'I teach Class ' + this.getClassNumber() + '.';
+        }
+        return result;
+    }
+
+    getClassNumber() {
+        return this.clazzes.reduce((clazz, val) => {
+                    clazz.push(val.number);
+                    return clazz;
+                    }, []).join(',');
+    }
+}
+
